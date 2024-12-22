9acb2433160e9035ce2cac185ee5166f4de841cb
patelaaman <amanpatel51251@gmail.com>
2024-12-22 22:54:10 +0530

    change something

--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -4,9 +4,9 @@ import { assets } from '../../assets/assets'
 const Navbar = () => {
   return (
     <div className='navbar'>
-        <img className="logo" src={assets.logo} alt="" />
+        <img className="logo" src={assets.logo} alt="" />   
         <img className="profile" src={assets.Profile} alt="" />
-      
+        
     </div>
   )
 }
