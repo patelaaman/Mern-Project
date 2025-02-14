4aea23c2c7972f128a976d47f3f711a48fdac534
patelaaman <amanpatel51251@gmail.com>
2025-02-14 21:38:57 +0530

    Coplete the Project

--- admin/src/pages/Order/Order.jsx
+++ admin/src/pages/Order/Order.jsx
@@ -21,6 +21,16 @@ const Order = ({url}) => {
     }
   }
 
+  const statusHandler = async (event,orderId) =>{
+         const response = await axios.post(url+"/api/order/status",{
+          orderId,
+          status:event.target.value
+         })
+         if(response.data.success){
+             await fetch
+         }
+  }
+
   useEffect(()=>{
      fetchAllOrders();
   },[])
@@ -30,7 +40,7 @@ const Order = ({url}) => {
            <div className="order-list">
                {orders.map((order,index)=>(
                 <div key={index} className="order-item">
-                <img src={assets.parcel}></img>
+                <img src={assets.parcel} height="50px"></img>
                 <div>
                   <p className='order-item-food'>
                     {order.items.map((item,index)=>{
@@ -42,8 +52,22 @@ const Order = ({url}) => {
                       }
                     })}
                   </p>
+                  <p className="order-item-name">
+                    {order.address.firstName+" "+order.address.lastName}
+                  </p>
+                  <div className="order-item-address">
+                    <p>{order.address.street+","}</p>
+                    <p>{order.address.city+", "+order.address.state+", "+order.address.country+", "+order.address.zipcode}</p>
+                  </div>
+                  <p className="order-item-phone">{order.address.phone}</p>
                 </div>
-               
+                <p>Items : {order.items.length}</p>
+                <p>${order.amount}</p>
+                <select  onChange={(event)=>statusHandler(event,order._id)} value={order.status}>
+                  <option value="Food Processing">Food Processing</option>
+                  <option value="Out for Delivery">Out for delivary</option>
+                  <option value="Delivered">Delivered</option>
+                </select>
            </div> ))}
     </div>
     </div>
